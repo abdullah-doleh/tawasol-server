d0c669bac7297df56611e9cdf638661c885e3353
abdullah doleh <abdallahdoleh98@hotmail.com>
2024-05-01 14:26:41 +0300

    initial commit

--- config/db.js
+++ config/db.js
@@ -1,18 +1,35 @@
 
+// const mongoose = require("mongoose");
+
+// const config = require("config");
+
+// const db= config.get("mongoConnectionString");// to connect to mongodb atlas
+
+// const  connectDB = async()=>{
+//     try{
+//         await mongoose.connect(db);
+//         console.log("connected to mongodb");
+//     }catch(err){
+//         console.log(err.message);
+//         process.exit(1);
+//     }
+   
+// }
+// module.exports = connectDB; 
 const mongoose = require("mongoose");
 
-const config = require("config");
+require('dotenv').config(); // Load environment variables from .env file
 
-const db= config.get("mongoConnectionString");// to connect to mongodb atlas
+const db = process.env.MONGO_CONNECTION_STRING;
 
-const  connectDB = async()=>{
-    try{
+const connectDB = async () => {
+    try {
         await mongoose.connect(db);
-        console.log("connected to mongodb");
-    }catch(err){
-        console.log(err.message);
+        console.log("Connected to MongoDB");
+    } catch (err) {
+        console.error("Error connecting to MongoDB:", err.message);
         process.exit(1);
     }
-   
-}
+};
+
 module.exports = connectDB;
